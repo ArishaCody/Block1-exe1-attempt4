db6bcde346427895322086deadb187d6940d95ad
ArishaCody <105153479+ArishaCody@users.noreply.github.com>
2022-05-14 20:20:34 +1000

    Добавьте файлы проекта.

---
+++ Block1 exe1 attempt4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Block1_exe1_attempt4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isSunny = false;
+            int coins = -214;
+            char symbol1 = 'G';
+            string greet = "Hi! My name is Bobby!";
+            uint money = -100000;
+            byte keyGold = 250;
+            long dollars = 0;
+            double smallCredit3 = 22.2;
+            float wayLong = 10.0f;
+            short card = -30;
+            sbyte cats = 127;
+        }
+    }
+}
